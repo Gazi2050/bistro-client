9f581fe297c5c2aa9c69fd9e69f6419215b5da63
Gazi2050 <gazi45344@gmail.com>
2023-11-10 07:46:46 +0600

    added navbar,banner,footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,14 +3,14 @@ const Footer = () => {
     return (
         <div>
             <div className=" text-white flex justify-center ">
-                <div className=" bg-slate-700 w-1/2 text-center space-y-3 py-5">
+                <div className=" bg-slate-700 w-1/2 text-center space-y-3 p-4">
                     <h3 className="text-2xl">CONTACT US</h3>
-                    <p>123 ABS Street, Uni 21, Bangladesh</p>
-                    <p>+88 123456789</p>
-                    <p>Mon - Fri: 08:00 - 22:00</p>
-                    <p>Sat - Sun: 10:00 - 23:00</p>
+                    <p className="text-xs">123 ABS Street, Uni 21, Bangladesh</p>
+                    <p className="text-xs">+88 123456789</p>
+                    <p className="text-xs">Mon - Fri: 08:00 - 22:00</p>
+                    <p className="text-xs">Sat - Sun: 10:00 - 23:00</p>
                 </div>
-                <div className=" bg-slate-900 w-1/2 text-center space-y-3 py-5">
+                <div className=" bg-slate-900 w-1/2 text-center space-y-3 p-4">
                     <h3 className="text-2xl">Follow US</h3>
                     <p>Join us on social media</p>
                     <div className="flex justify-center space-x-3">
@@ -22,7 +22,7 @@ const Footer = () => {
             </div>
             <div className="footer footer-center p-4 bg-black text-white">
                 <aside>
-                    <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+                    <p>Copyright © 2023 CulinaryCloud. All rights reserved.</p>
                 </aside>
             </div>
         </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Banner from "../Banner/Banner";
 
 
 const Home = () => {
     return (
         <div>
-            <h1 className='text-center text-4xl font-bold'>This is Home</h1>
+            {/* <h1 className='text-center text-4xl font-bold'>This is Home</h1> */}
+            <Banner></Banner>
         </div>
     );
 };
