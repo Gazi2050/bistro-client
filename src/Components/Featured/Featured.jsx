99fc5abbb48f765ff66f4a1a07bf0dd00fa5fa78
Gazi2050 <gazi45344@gmail.com>
2023-11-12 19:46:22 +0600

    update Featured

--- src/Components/Featured/Featured.jsx
+++ src/Components/Featured/Featured.jsx
@@ -3,7 +3,7 @@ import featuredImg from "../../assets/home/featured.jpg"
 
 const Featured = () => {
     return (
-        <div style={{ backgroundImage: `url(${featuredImg})` }}>
+        <div style={{ backgroundImage: `url(${featuredImg})` }} className="bg-fixed">
 
             <div className="text-white hero-overlay bg-opacity-60 p-10">
                 <SectionTitle
@@ -18,7 +18,7 @@ const Featured = () => {
                         <p>March 20, 2023</p>
                         <p className="uppercase">WHERE CAN I GET SOME?</p>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptate facere, deserunt dolores maiores quod nobis quas quasi. Eaque repellat recusandae ad laudantium tempore consequatur consequuntur omnis ullam maxime tenetur.</p>
-                        <button className="btn btn-outline text-yellow-300">Read More</button>
+                        <button className="btn btn-outline text-yellow-300 border-b-4 border-0 backdrop-blur-2xl">Read More</button>
                     </div>
                 </div>
             </div>
